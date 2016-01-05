ad8b3b806b719b3aaa1fd550cdfb61f9cfc2fd1d
Eric Retzloff <eretzloff@gmail.com>
2016-01-04 20:26:02 -0500

    Completed exercise 7

---
+++ ex06.js
@@ -0,0 +1,24 @@
+var Hapi = require('hapi')
+var H2o2 = require('h2o2')
+
+var server = new Hapi.Server()
+
+server.register(H2o2, function() {} )
+
+server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+})
+
+server.route({
+    path: '/proxy',
+    method: 'GET',
+    handler: {
+        proxy: {
+            host: '127.0.0.1',
+            port: 65535
+        }
+    }
+})
+
+server.start( function() {} )
