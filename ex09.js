ec54b04caefaad47aa16eee0ae16090356863a74
Eric Retzloff <eretzloff@gmail.com>
2016-01-06 21:15:36 -0500

    Completed exercise 9

---
+++ ex09.js
@@ -0,0 +1,26 @@
+var Hapi = require('hapi')
+var Joi  = require('joi')
+
+var server = new Hapi.Server()
+
+server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+})
+
+server.route({
+    path:     '/chickens/{breed}',
+    method:   'GET',
+    handler:  function(request, reply){
+        return reply(request.params.breed + 'chickens!' )
+    },
+    config:  {
+        validate: {
+            params: {
+                breed: Joi.string().required()
+            }
+        }
+    }
+})
+
+server.start(function(){})
